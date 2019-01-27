1f179231e13103656629075696a76a2372b4517a
Mateus Marques <marques.m05@gmail.com>
2019-01-27 00:07:58 -0200

    Make UI Tests for all operations and failure

--- UITestPresentation/ViewController.swift
+++ UITestPresentation/ViewController.swift
@@ -17,14 +17,14 @@ class ViewController: UIViewController {
 		field.borderStyle = .roundedRect
 		return field
 	}()
-
-  private(set) lazy var value2TextField: UITextField = {
-    let field = UITextField()
-    field.translatesAutoresizingMaskIntoConstraints = false
-    field.accessibilityIdentifier = "value2TextField"
-    field.borderStyle = .roundedRect
-    return field
-  }()
+	
+	private(set) lazy var value2TextField: UITextField = {
+		let field = UITextField()
+		field.translatesAutoresizingMaskIntoConstraints = false
+		field.accessibilityIdentifier = "value2TextField"
+		field.borderStyle = .roundedRect
+		return field
+	}()
 	
 	private(set) lazy var symbolLabel: UILabel = {
 		let label = UILabel()
@@ -33,23 +33,23 @@ class ViewController: UIViewController {
 		label.accessibilityIdentifier = "symbolLabel"
 		return label
 	}()
-
-  private(set) lazy var equalLabel: UILabel = {
-    let label = UILabel()
-    label.translatesAutoresizingMaskIntoConstraints = false
-    label.textAlignment = .center
-    label.accessibilityIdentifier = "equalLabel"
-    label.text = "="
-    return label
-  }()
-
-  private(set) lazy var resultLabel: UILabel = {
-    let label = UILabel()
-    label.translatesAutoresizingMaskIntoConstraints = false
-    label.textAlignment = .center
-    label.accessibilityIdentifier = "resultLabel"
-    return label
-  }()
+	
+	private(set) lazy var equalLabel: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.textAlignment = .center
+		label.accessibilityIdentifier = "equalLabel"
+		label.text = "="
+		return label
+	}()
+	
+	private(set) lazy var resultLabel: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.textAlignment = .center
+		label.accessibilityIdentifier = "resultLabel"
+		return label
+	}()
 	
 	private(set) lazy var button: UIButton = {
 		let button = UIButton()
@@ -59,28 +59,28 @@ class ViewController: UIViewController {
 		button.setTitle("Calcular", for: .normal)
 		button.setTitleColor(UIColor.white, for: .normal)
 		button.backgroundColor = .darkGray
-    button.layer.cornerRadius = 4
-    button.clipsToBounds = true
+		button.layer.cornerRadius = 4
+		button.clipsToBounds = true
 		return button
 	}()
-
-  private(set) lazy var fieldsStackView: UIStackView = {
-    let stackView = UIStackView()
-    stackView.translatesAutoresizingMaskIntoConstraints = false
-    stackView.axis = .horizontal
-    stackView.distribution = .fillEqually
-    stackView.spacing = 20
-    return stackView
-  }()
-
-  private(set) lazy var operationsStackView: UIStackView = {
-    let stackView = UIStackView()
-    stackView.translatesAutoresizingMaskIntoConstraints = false
-    stackView.axis = .horizontal
-    stackView.distribution = .fillEqually
-    stackView.spacing = 20
-    return stackView
-  }()
+	
+	private(set) lazy var fieldsStackView: UIStackView = {
+		let stackView = UIStackView()
+		stackView.translatesAutoresizingMaskIntoConstraints = false
+		stackView.axis = .horizontal
+		stackView.distribution = .fillEqually
+		stackView.spacing = 20
+		return stackView
+	}()
+	
+	private(set) lazy var operationsStackView: UIStackView = {
+		let stackView = UIStackView()
+		stackView.translatesAutoresizingMaskIntoConstraints = false
+		stackView.axis = .horizontal
+		stackView.distribution = .fillEqually
+		stackView.spacing = 20
+		return stackView
+	}()
 	
 	private(set) lazy var errorAlert: UIAlertController = {
 		let alert = UIAlertController(title: "Ops", message: "", preferredStyle: .alert)
@@ -88,10 +88,10 @@ class ViewController: UIViewController {
 		alert.addAction(action)
 		return alert
 	}()
-
-  let mathOperation = MathOperation()
-  var currentOperation: OperationType?
-
+	
+	let mathOperation = MathOperation()
+	var currentOperation: OperationType?
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
@@ -108,87 +108,87 @@ class ViewController: UIViewController {
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
 		setupConstraints()
-    addOperationsButton()
+		addOperationsButton()
 	}
-
+	
 	@objc func action() {
-    guard let operation = currentOperation else {
-      alert(with: .symbolError)
-      return
-    }
-    guard let value1String = value1TextField.text else {
-      alert(with: .valueError)
-      return
-    }
-    guard let value2String = value2TextField.text else {
-      alert(with: .valueError)
-      return
-    }
-    guard let value1 = Double(value1String) else {
-      alert(with: .unknown)
-      return
-    }
-    guard let value2 = Double(value2String) else {
-      alert(with: .unknown)
-      return
-    }
-
-    let result = mathOperation.result(operation: operation, value1: value1, value2: value2)
-    resultLabel.text = String(result)
+		guard let operation = currentOperation else {
+			alert(with: .symbolError)
+			return
+		}
+		guard let value1String = value1TextField.text else {
+			alert(with: .valueError)
+			return
+		}
+		guard let value2String = value2TextField.text else {
+			alert(with: .valueError)
+			return
+		}
+		guard let value1 = Double(value1String) else {
+			alert(with: .unknown)
+			return
+		}
+		guard let value2 = Double(value2String) else {
+			alert(with: .unknown)
+			return
+		}
+		
+		let result = mathOperation.result(operation: operation, value1: value1, value2: value2)
+		resultLabel.text = String(result)
 	}
-
-  @objc func operationAction(_ button: OperationButton) {
-    currentOperation = button.operationType
-    symbolLabel.text = button.operationType.rawValue
-  }
-
-  private func addOperationsButton() {
-    for operation in OperationType.allCases {
-      let button = OperationButton(operationType: operation)
-      button.translatesAutoresizingMaskIntoConstraints = false
-      button.setTitle(operation.rawValue, for: .normal)
-      button.accessibilityLabel = "Operação: \(operation.rawValue)"
-      button.accessibilityIdentifier = "op_\(operation.rawValue)"
-      button.addTarget(self, action: #selector(operationAction(_:)), for: .touchUpInside)
-      operationsStackView.addArrangedSubview(button)
-    }
-  }
-
-  private func alert(with message: ErrorMessage) {
-	errorAlert.message = message.rawValue
-    present(errorAlert, animated: true, completion: nil)
-  }
+	
+	@objc func operationAction(_ button: OperationButton) {
+		currentOperation = button.operationType
+		symbolLabel.text = button.operationType.rawValue
+	}
+	
+	private func addOperationsButton() {
+		for operation in OperationType.allCases {
+			let button = OperationButton(operationType: operation)
+			button.translatesAutoresizingMaskIntoConstraints = false
+			button.setTitle(operation.rawValue, for: .normal)
+			button.accessibilityLabel = "Operação: \(operation.rawValue)"
+			button.accessibilityIdentifier = "op_\(operation.rawValue)"
+			button.addTarget(self, action: #selector(operationAction(_:)), for: .touchUpInside)
+			operationsStackView.addArrangedSubview(button)
+		}
+	}
+	
+	private func alert(with message: ErrorMessage) {
+		errorAlert.message = message.rawValue
+		present(errorAlert, animated: true, completion: nil)
+	}
 	
 	private func setupConstraints() {
-    NSLayoutConstraint.activate([
-      fieldsStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
-      fieldsStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      fieldsStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
-      fieldsStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
-      fieldsStackView.heightAnchor.constraint(equalToConstant: 40)
-      ])
-
-    NSLayoutConstraint.activate([
-      operationsStackView.topAnchor.constraint(equalTo: value1TextField.bottomAnchor, constant: 20),
-      operationsStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      operationsStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
-      operationsStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
-      operationsStackView.heightAnchor.constraint(equalToConstant: 40)
-      ])
+		NSLayoutConstraint.activate([
+			fieldsStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
+			fieldsStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+			fieldsStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+			fieldsStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
+			fieldsStackView.heightAnchor.constraint(equalToConstant: 40)
+			])
+		
+		NSLayoutConstraint.activate([
+			operationsStackView.topAnchor.constraint(equalTo: value1TextField.bottomAnchor, constant: 20),
+			operationsStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+			operationsStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+			operationsStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
+			operationsStackView.heightAnchor.constraint(equalToConstant: 40)
+			])
 		
 		NSLayoutConstraint.activate([
 			button.heightAnchor.constraint(equalToConstant: 40),
 			button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
 			button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
 			button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
-//      		button.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40)
-      button.topAnchor.constraint(equalTo: operationsStackView.bottomAnchor, constant: 40)
+			//      		button.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40)
+			button.topAnchor.constraint(equalTo: operationsStackView.bottomAnchor, constant: 40)
 			])
 	}
 }
 
 enum ErrorMessage: String {
-  case symbolError = "Escolha a operação que deseja realiar"
-  case valueError = "Digite os valores corretamente"
-  case unknown = "Algo de errado não está certo"
+	case symbolError = "Escolha a operação que deseja realiar"
+	case valueError = "Digite os valores corretamente"
+	case unknown = "Algo de errado não está certo"
 }
--- UITestPresentationUITests/UITestPresentationUITests.swift
+++ UITestPresentationUITests/UITestPresentationUITests.swift
@@ -21,8 +21,7 @@ class UITestPresentationUITests: XCTestCase {
 		
 	}
 	
-	func testExample() {
-		
+	func testSum() {
 		app.launch()
 		let value1TextField = app.textFields["value1TextField"]
 		value1TextField.tap()
@@ -43,5 +42,87 @@ class UITestPresentationUITests: XCTestCase {
 		
 		let result = app.staticTexts["3.0"]
 		XCTAssert(result.exists)
+	}
+	
+	func testDiv() {
+		app.launch()
+		let value1TextField = app.textFields["value1TextField"]
+		value1TextField.tap()
+		value1TextField.typeText("10")
+		
+		let value2TextField = app.textFields["value2TextField"]
+		value2TextField.tap()
+		value2TextField.typeText("2")
+		
+		let opButton = app.buttons["op_/"]
+		opButton.tap()
+		
+		let symbol = app.staticTexts["/"]
+		XCTAssert(symbol.exists)
+		
+		let calcButton = app.buttons["button"]
+		calcButton.tap()
+		
+		let result = app.staticTexts["5.0"]
+		XCTAssert(result.exists)
+	}
+	
+	func testSub() {
+		app.launch()
+		let value1TextField = app.textFields["value1TextField"]
+		value1TextField.tap()
+		value1TextField.typeText("2")
+		
+		let value2TextField = app.textFields["value2TextField"]
+		value2TextField.tap()
+		value2TextField.typeText("1")
+		
+		let opButton = app.buttons["op_-"]
+		opButton.tap()
+		
+		let symbol = app.staticTexts["-"]
+		XCTAssert(symbol.exists)
+		
+		let calcButton = app.buttons["button"]
+		calcButton.tap()
+		
+		let result = app.staticTexts["1.0"]
+		XCTAssert(result.exists)
+	}
+	
+	func testMult() {
+		app.launch()
+		let value1TextField = app.textFields["value1TextField"]
+		value1TextField.tap()
+		value1TextField.typeText("2")
+		
+		let value2TextField = app.textFields["value2TextField"]
+		value2TextField.tap()
+		value2TextField.typeText("2")
+		
+		let opButton = app.buttons["op_*"]
+		opButton.tap()
+		
+		let symbol = app.staticTexts["*"]
+		XCTAssert(symbol.exists)
+		
+		let calcButton = app.buttons["button"]
+		calcButton.tap()
+		
+		let result = app.staticTexts["4.0"]
+		XCTAssert(result.exists)
+	}
+	
+	func testFailure() {
+		app.launch()
+		
+		let calcButton = app.buttons["button"]
+		calcButton.tap()
+		
+		let alert = app.alerts
+		XCTAssert(alert.count == 1)
+		
+		let errorText = alert.staticTexts["Escolha a operação que deseja realiar"]
+		XCTAssert(errorText.exists)
 	}
 }
